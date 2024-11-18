349cdf4e8e2e5ad6ea9724d2e3e585251272c508
fkpe <ilasavic511@gmail.com>
2024-11-18 20:57:59 +0300

    PRO-3: - add dotenv

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}/`);
 });
