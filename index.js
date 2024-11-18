28e7979efb5dab3746f08b56473edc05a656f092
fkpe <ilasavic511@gmail.com>
2024-11-18 20:37:15 +0300

    PRO-2: - add Express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
