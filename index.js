8662a087a8c83cac4a889e0b875f61881d37328e
fkpe <ilasavic511@gmail.com>
2024-11-19 10:08:56 +0300

    PRO-4: - add end-point

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const app = express();
 const dotenv = require('dotenv');
 
 dotenv.config();
-const app = express();
 const port = process.env.PORT || 3000;
+const apiKey = process.env.NASA_API_KEY;
+
+
+app.get('/meteors', (req, res) => {
+    const startDate = '2024-11-11';
+    const endDate = '2024-11-15';
+    const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`;
+
+    fetch(url)
+        .then((response) => response.json())
+            .then((data) => {
+                res.send(data);
+            })
+        .catch((error) => {
+            console.error('Error fetching data from NASA API:', error);
+        });
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -12,3 +29,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}/`);
 });
+
+
+
+
