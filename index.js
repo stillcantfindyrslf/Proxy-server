501c3a68b4538c9e08ffd10e30e1e16ec66f2257
fkpe <ilasavic511@gmail.com>
2024-11-19 12:23:13 +0300

    PRO-6: - add data filter

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 const express = require('express');
-const app = express();
 const dotenv = require('dotenv');
 
 dotenv.config();
+const app = express();
 const port = process.env.PORT || 3000;
 const apiKey = process.env.NASA_API_KEY;
+const startDate = '2024-11-11';
+const endDate = '2024-11-15';
 
 
-app.get('/meteors', (req, res) => {
-    const startDate = '2024-11-11';
-    const endDate = '2024-11-15';
-    const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`;
-
-    fetch(url)
-        .then((response) => response.json())
-            .then((data) => {
-                res.send(data);
-            })
-        .catch((error) => {
-            console.error('Error fetching data from NASA API:', error);
-        });
+app.get('/', async (req, res) => {
+    res.send('Hello World!');
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/meteors', async (req, res) => {
+    const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const filteredData = [];
+    for (const date in data.near_earth_objects) {
+        data.near_earth_objects[date].forEach(asteroid => {
+            filteredData.push({
+                id: asteroid.id,
+                name: asteroid.name,
+                diameter: asteroid.estimated_diameter.meters.estimated_diameter_max,
+                is_potentially_hazardous_asteroid: asteroid.is_potentially_hazardous_asteroid,
+                close_approach_date_full: asteroid.close_approach_data[0].close_approach_date_full,
+                relative_velocity: asteroid.close_approach_data[0].relative_velocity.kilometers_per_second
+            });
+        });
+    }
+    res.json(filteredData);
 });
 
 app.listen(port, () => {
