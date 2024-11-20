f91f4121ed9ac0eafea55e5d3a26290990329b3e
fkpe <ilasavic511@gmail.com>
2024-11-20 22:32:09 +0300

    PRO-11: - add sentry

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const dotenv = require('dotenv');
+require("./instrument.js");
+const Sentry = require("@sentry/node");
+
 dotenv.config();
 
 const app = require('./delivery/app');
+
+Sentry.setupExpressErrorHandler(app);
+
+app.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+});
 
 const port = process.env.PORT || 3000;
 
